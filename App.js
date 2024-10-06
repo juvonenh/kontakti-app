da1ef0e54cb2c0396b2675eafacb3634e03741a5
Hanna Juvonen <hanna.juvonen@myy.haaga-helia.fi>
2024-10-06 21:05:22 +0300

    Tehtavan palautus

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button, FlatList } from "react-native";
+import * as Contacts from "expo-contacts";
 
 export default function App() {
+  const [contact, setContact] = useState([]);
+  // console.log(contact);
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === "granted") {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers],
+        pageSize: 10,
+      });
+      if (data.length > 0) {
+        console.log(data);
+        setContact(data);
+        // console.log(data[0]);
+        // setContact(data[0]);
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={contact}
+        renderItem={({ item }) => (
+          <Text>
+            {item.name} {[item.phoneNumbers[0].number]}
+          </Text>
+        )}
+      />
+      {/* <Text>
+        {contact.name} {[contact.phoneNumbers[0].number]}
+      </Text> */}
+      <Button title="Get Contacts" onPress={getContacts} />
     </View>
   );
 }
@@ -13,8 +44,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 100,
+    paddingBottom: 50,
   },
 });
